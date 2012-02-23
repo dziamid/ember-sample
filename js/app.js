1f58308e233c02a5d987d212c4824504172f79b3
Dziamid <dziamid@gmail.com>
2012-02-23 13:07:36 +0300

    added twig templates

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 var App = Em.Application.create();
 
 App.MyView = Em.View.extend({
-  mouseDown: function() {
-    window.alert("hello world!");
-  }
+    mouseDown: function () {
+        window.alert("hello world!");
+    }
 });
 
 App.ButtonView = Em.View.extend({
@@ -12,3 +12,50 @@ App.ButtonView = Em.View.extend({
         this.$().button();
     }
 });
+
+App.Tags = [
+    "ActionScript",
+    "AppleScript",
+    "Asp",
+    "BASIC",
+    "C",
+    "C++",
+    "Clojure",
+    "COBOL",
+    "ColdFusion",
+    "Erlang",
+    "Fortran",
+    "Groovy",
+    "Haskell",
+    "Java",
+    "JavaScript",
+    "Lisp",
+    "Perl",
+    "PHP",
+    "Python",
+    "Ruby",
+    "Scala",
+    "Scheme"
+];
+
+App.InputView = Em.View.extend({
+    tagName: 'input',
+    didInsertElement: function () {
+        this.$().autocomplete({
+            source: App.Tags
+        });
+    }
+});
+
+App.CustomView = Em.View.extend({
+    templateName: 'custom-1',
+    value: 'Bla'
+});
+
+var twigTemplate1 = twig({
+    data: 'The {{ value }} is a lie.'
+});
+
+Ember.TEMPLATES["custom-1"] = function (context) {
+    return twigTemplate1.render(context);
+};
