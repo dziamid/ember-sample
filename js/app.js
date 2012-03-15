4a79ee7f237643e5d6931323fa360018167dcc9c
Dziamid <dziamid@gmail.com>
2012-03-15 17:39:06 +0300

    added filter example

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
 var App = Em.Application.create({
     ready: function () {
         App.get('People').set('content', App.store.findAll(App.Person));
+        App.store.createRecord(App.Person, {age: 50, name: 'Paul'});
+        App.store.createRecord(App.Person, {age: 25, name: 'Paul'});
     }
 });
 
@@ -29,4 +31,7 @@ App.set('People', Ember.ArrayProxy.create({
     content: []
 }));
 
+App.set('OldPeople', App.store.filter(App.Person, function (data) {
+    return data.age > 29;
+}));
 
